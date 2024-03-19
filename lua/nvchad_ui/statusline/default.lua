8a2c9704725e7aa048765faf626538d460a3392d
IsaacShelton <isaac.a.shelton@gmail.com>
2024-03-19 14:14:28 -0500

    Added time clock

--- lua/nvchad_ui/statusline/default.lua
+++ lua/nvchad_ui/statusline/default.lua
@@ -164,7 +164,7 @@ end
 
 M.cursor_position = function()
   local left_sep = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
-  local left_sep_clock = "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon#" .. " "
+  local left_sep_clock = "%#St_NormalMode#" .. sep_l .. "%#St_pos_icon#" .. " "
 
   local current_line = fn.line "."
   local total_line = fn.line "$"
