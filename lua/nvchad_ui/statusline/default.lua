31a275a850a7e9d44143e669bda390a2b280fe3b
IsaacShelton <isaac.a.shelton@gmail.com>
2024-03-19 14:18:00 -0500

    Added time clock

--- lua/nvchad_ui/statusline/default.lua
+++ lua/nvchad_ui/statusline/default.lua
@@ -174,7 +174,7 @@ M.cursor_position = function()
   text = (current_line == 1 and "Top") or text
   text = (current_line == total_line and "Bot") or text
 
-  return left_sep .. "%#St_pos_text#" .. " " .. text .. " " .. left_sep_clock .. "%#St_pos_text#" .. os.date("%I:%M") .. " "
+  return left_sep .. "%#St_pos_text#" .. " " .. text .. " " .. left_sep_clock .. "%#St_pos_text#" .. " " .. os.date("%I:%M") .. " "
   -- return left_sep .. "%#St_pos_text#" .. " " .. text .. " "
 end
 
